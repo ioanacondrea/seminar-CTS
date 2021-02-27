836f09ddaf169b76a24bb65c4035ff0e52464cec
condreaioana <condreaioana18@stud.ase.ro>
2021-02-27 14:21:26 +0200

    clasele Animal si Zookeeper au fost adaugate

---
+++ Seminar1/src/cts/seminar/classes/Zookeeper.java
@@ -0,0 +1,27 @@
+package cts.seminar.classes;
+
+public class Zookeeper {
+
+	private String nume;
+	
+
+	public Zookeeper(String nume) {
+		super();
+		this.nume = nume;
+	}
+
+	public String getNume() {
+		return nume;
+	}
+
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+	
+	public void feed(Animal a){
+		System.out.println("Ingrijitorul " + this.nume+ " hraneste animalul " + a.getNume());
+		
+	}
+	
+	
+}
