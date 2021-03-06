480eb9d2c859ec252f3f1ab5ce80d77c2b70f709
condreaioana <condreaioana18@stud.ase.ro>
2021-03-06 14:36:29 +0200

    inlocuire cu clasa abstracta Aplicant

--- Seminar2/src/ro/ase/cts/program/Program.java
+++ Seminar2/src/ro/ase/cts/program/Program.java
@@ -4,15 +4,18 @@ import java.io.FileNotFoundException;
 import java.util.List;
 
 import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.Aplicant;
 import ro.ase.cts.clase.Utils;
+import ro.ase.cts.readers.ReaderAngajat;
+import ro.ase.cts.readers.ReaderElev;
 
 public class Program {
 
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaAplicant;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
+			listaAplicant = ReaderElev.readPupil("elevi.txt");
+			for(Aplicant angajat:listaAplicant)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
